27748f9963ed28d1c921706ff7f3c6efc6672d38
buddhini81 <buddhinisam@hotmail.com>
2020-04-28 19:59:12 +0530

    Module 3 implementation

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -217,10 +217,16 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return function (product) {
+        return product.id == id;
+    }
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return Math.floor(Math.random() * 20) + 1;
+};
 
 
 const getProduct = (prodList, pId) => {
