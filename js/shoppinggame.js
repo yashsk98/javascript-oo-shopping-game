936f4a78a4f815cf0f1b741dbaefb1dbe20430db
Yash Kulkarni <yash.kulkarni@salesforce.com>
2020-10-06 15:40:17 +0530

    4 done

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -219,7 +219,7 @@ const getProduct = (prodList, pId) => {
 
 
 // Complete this function
-const calculateBill = (prod, tBill) => {};
+const calculateBill = (prod, tBill) => {return tBill + prod.price};
 
 const findPointsToBill = (roundedTotal) => {
     if (roundedTotal > 10 && roundedTotal <= 100) {
@@ -241,12 +241,21 @@ const findPointsToBill = (roundedTotal) => {
 
 
 // Complete this function
-const findPointsForExpDate = (prod) => {};
+const findPointsForExpDate = (prod) => {return (prod.daysToExpire < 30) ? 10 : 0};
 
 
 const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
     let pointsForExpDate = findPointsForExpDate(prod);
+    player.score = player.score + pointsToBill + pointsForExpDate;
+
+    if(prod instanceof MagicProduct){
+        if(prod.isBonus){
+            player.addPoints(prod.points);
+        }else{
+            player.deductPoints(prod.points);
+        }
+    }
 };
 
 // Complete this function
